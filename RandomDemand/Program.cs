72031fca544702e6a5fd9d3b343f0de9ef47f69b
Erko Targamaa <erkotargamaa@gmail.com>
2022-12-01 11:01:09 +0200

    week6

---
+++ RandomDemand/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+
+Random random = new Random();
+
+int randomIndex = random.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
+
